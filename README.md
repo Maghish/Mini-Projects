0a8630471041a4841ebd66d6ed2e2822cc72f4f7
Maghish <132087686+Maghish@users.noreply.github.com>
2023-06-11 13:56:15 +0530

    Added the Pythonic Billing project files
    This is a project idea for my CS50p final project.

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
-# Mini_Projects
-This is a collection of my mini projects that I used to do way back when I started Python.
+# The Pythonic Billing System
+#### Video Demo:  <URL HERE>
+## Description: 
+This is a simple billing system which is designed by Python language. You can add items, search items in `Admin` mode and in `Customers` mode, you can search for available products and if you gave the exact name of the product, it will ask you to whether add it to the cart of yours. At last you can type the keyword "**`done`**" to end the search and return you the total price of the products in the cart.
---
+++ index.py
@@ -0,0 +1,182 @@
+import csv 
+
+
+class Admin():
+    
+    def search(self, item):
+         Items = []
+         with open("items.csv", "r") as file:
+             reader = csv.DictReader(file)
+             for row in reader:
+                 if row['item_name'] == item: 
+                      price = row['item_price'].replace("$", "")
+                      return [price, 1] 
+                 elif row['item_name'].startswith(item) or item in row['item_name']: 
+                      Items.append(row['item_name'])
+                 else:
+                     pass
+         if len(Items) == 0:
+            return False
+         else:
+            return [Items, 0]
+             
+    def add(self, item, price):
+        with open("items.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row['item_name'] == item: 
+                    return False
+                else:
+                    pass
+        with open("items.csv", "a", newline='') as file:
+            writer = csv.DictWriter(file, fieldnames=["item_name", "item_price"])
+            writer.writerow({"item_name": item.rstrip(), "item_price": price.rstrip()}) 
+
+class Customer():
+
+    def search(self, item):
+
+        Items = []
+        with open("items.csv", "r") as file:
+            reader = csv.DictReader(file)
+            for row in reader:
+                if row['item_name'] == item: 
+                    price = row['item_price'].replace("$", "")
+                    return [price, 1] 
+                elif row['item_name'].startswith(item) or item in row['item_name']:
+                    Items.append(row['item_name'])
+                else:
+                    pass
+        if len(Items) == 0:
+            return False
+        else:
+            return [Items, 0]
+
+
+
+
+      
+def main():
+    while True:
+        mode = input('''\n\nModes:
+1. Admin
+2. Customer
+3. Exit 
+Select a option above: ''')
+        
+        if mode == '1' or mode.lower() == "admin":
+            admin()
+        elif mode == '2' or mode.lower() == "customer":
+            customer()
+
+        elif mode == '3' or mode.lower() == "exit":
+            exit("Shutdowning.... ")    
+        else: 
+            print("Invalid option")
+            pass    
+
+
+
+def admin():
+    while True:
+            operation = input('''\n\nActions:
+1. Add items 
+2. Search  
+3. Back
+4. Exit
+Select a option above: ''')
+            if operation == '1' or operation.lower() == "add items":
+                while True:
+                    result = input("Type as (Item_name, Item_price): ")
+                    if result.lower() == "back":
+                        break
+                    if result.lower() == "exit":
+                        exit()
+                    try:
+                        name, price = result.split(",")
+                        admin_item = Admin()
+                        if price.strip().startswith("$"):
+                            price = price.strip()
+                        else:
+                            price = f"${price.strip()}"
+                        admin_item.add(name, price)
+
+                    except: 
+                        print("Try again!")
+                        pass
+                
+            elif operation == '2' or operation.lower() == "search":
+                while True:
+                    item_name = input("Item name: ")
+                    if item_name.lower() == "exit":
+                        exit("Shutdowning.... ")
+                    if item_name.lower() == "back":
+                        break
+                    admin_item = Admin()
+                    result = admin_item.search(item_name)
+                    if result == False:
+                        print("No Results found")
+                        pass
+                    elif result[1] == 0:
+                        i = 0
+                        for items in result[0]:
+                            print(f"{i + 1}.{items}")
+                            i += 1
+                        print(f"{i} results found")
+                    elif result[1] == 1:
+                        print(f"Item: {item_name}\nPrice: ${result[0]}")    
+            
+            elif operation == '3' or operation.lower() == "back":
+                break
+
+            elif operation == '4' or operation.lower() == "exit":
+                exit("Shutdowning.... ")
+            
+    
+
+
+
+
+def customer():
+    Items = 0
+    while True:
+        item = input("Item: ")
+        if item.lower() == "exit":
+            exit("Shutdowning.... ")
+        if item.lower() == "back":
+            break
+        if item.lower() == "done":
+            if Items == 0:
+                exit("No items added\nThank You! Visit again!")
+            else:
+                exit(f"Total: ${Items}\nThank You! Visit again!")    
+        customer_item = Customer()
+        result = customer_item.search(item)
+
+        if result == False:
+            print("No Results found")
+            pass
+        else:
+            if result[1] == 0:
+                i = 0
+                for items in result[0]:
+                    print(f"{i + 1}.{items}")
+                    i += 1
+                print(f"{i} results found")
+            elif result[1] == 1:
+                print(f"Price: ${result[0]}")
+                ask = input("Add to cart? Y/N ")
+                if ask.lower() == "y":
+                    print(f"{item} added to cart")
+                    Items += int(result[0])
+                    print(f"${Items}")
+                elif ask.lower() == "n":
+                    pass
+                else:
+                    print("Invalid Input")
+                    pass 
+            
+
+        
+if __name__ == "__main__":
+    main()   
